c67033483354486f12474ff6744754a5ace918c5
bnnovdg <bru.rivas@duocuc.cl>
2026-05-03 23:56:38 -0400

    feat: crea controladores para la gestión de etiquetas

---
+++ src/controller/tagsController.js
@@ -0,0 +1,37 @@
+const tagsService = require('../service/tagsService');
+
+class TagsController {
+
+    // Lista tags del estudiante
+    async getTags(req, res, next) {
+        try {
+            const tags = await tagsService.getTags(req.userId);
+            res.status(200).json(tags);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // Crea una nueva tag
+    async createTag(req, res, next) {
+        try {
+            const tag = await tagsService.createTag(req.body, req.userId);
+            res.status(201).json(tag);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // Elimina una tag
+    async deleteTag(req, res, next) {
+        try {
+            const { id } = req.params;
+            await tagsService.deleteTag(parseInt(id), req.userId);
+            res.status(204).send();
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new TagsController();
